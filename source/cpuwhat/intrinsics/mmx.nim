5b1c9f4dc6a1bde450a5a7536e2cbd36dc3a8b4a
awr1 <41453959+awr1@users.noreply.github.com>
2020-04-22 14:52:09 -0500

    x86: Added MMX intrinsics

---
+++ source/cpuwhat/intrinsics/mmx.nim
@@ -0,0 +1,33 @@
+import
+  cpuwhat / private / consts,
+  nimterop / build,
+  nimterop / cimport,
+  std / os,
+  std / strutils,
+  std / sequtils
+
+when GCCLike:
+  const GCCFlags = "-mmmx"
+  {.passC: GCCFlags.}
+  {.passL: GCCFlags.}
+
+static:
+  cAddStdDir()
+
+cOverride:
+  type
+    m64_u* {.importc: "__$1", header: "<mmintrin.h>".} = object
+    m64*   {.importc: "__$1", header: "<mmintrin.h>".} = object
+    v2si   {.importc: "__$1", header: "<mmintrin.h>".} = object
+    v4hi   {.importc: "__$1", header: "<mmintrin.h>".} = object
+    v8qi   {.importc: "__$1", header: "<mmintrin.h>".} = object
+    v1di   {.importc: "__$1", header: "<mmintrin.h>".} = object
+    v2sf   {.importc: "__$1", header: "<mmintrin.h>".} = object
+
+cPlugin:
+  import std / strutils
+
+  proc onSymbol*(sym :var Symbol) {.exportc, dynlib.} =
+    sym.name = sym.name.strip(chars = {'_'})
+
+cImport(cSearchPath("mmintrin.h"), recurse = true, flags = ToastFlags)

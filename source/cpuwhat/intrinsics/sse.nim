909061434956ab268323644cb7a741d4976005f5
awr1 <41453959+awr1@users.noreply.github.com>
2020-04-22 17:13:57 -0500

    x86: Header is 'mm_malloc.h' not 'mm_alloc.h'

--- source/cpuwhat/intrinsics/sse.nim
+++ source/cpuwhat/intrinsics/sse.nim
@@ -47,5 +47,5 @@ cPlugin:
     sym.name = sym.name.strip(chars = {'_'})
 
 cImport(cSearchPath("mmintrin.h"),  flags = ToastFlags)
-cImport(cSearchPath("mm_alloc.h"),  flags = ToastFlags)
+cImport(cSearchPath("mm_malloc.h"), flags = ToastFlags)
 cImport(cSearchPath("xmmintrin.h"), flags = ToastFlags)

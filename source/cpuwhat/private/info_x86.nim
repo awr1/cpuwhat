7a971ae606c1be24e4c7ca830487b8cc17f0da4b
awr1 <41453959+awr1@users.noreply.github.com>
2021-05-01 14:06:47 -0500

    common: Module reorganization Put stuff like hasCongruentISA() in main module, other procs like currentAffinity() in new info_common module

--- source/cpuwhat/private/info_x86.nim
+++ source/cpuwhat/private/info_x86.nim
@@ -1,4 +1,5 @@
 import
+  info_common,
   std / strutils,
   std / cpuinfo,
   std / sequtils,
@@ -158,44 +159,6 @@ let
         featuresByAffinity[feature].setBit(affinity)
     featuresByAffinity
 
-template cached(expression :untyped) :bool =
-  let cache {.global.} = expression
-  expression
-
-proc currentAffinity() :int {.inline.} =
-  when defined(windows):
-    proc GetCurrentProcessorNumber() :uint32
-      {.importc, stdcall, dynlib: "Kernel32".}
-    GetCurrentProcessorNumber().int
-  else:
-    # TODO(awr1): Implement this on Linux/MacOS, etc.
-    discard
-
-proc hasCongruentISA*() :bool {.inline.} =
-  ## Reports `true` if the available instruction feature set is the same across
-  ## all cores. This does not necessarily mean the CPU cores are homogenous,
-  ## only that code should be relatively compatible regardless of the core it's
-  ## being executed on.
-  ##
-  ## Certain multi-core CPU packages are based on a heterogenous or "hybridized"
-  ## design where multiple cores in a system are based on differing hardware
-  ## designs to elicit different performance characteristics, e.g. CPUs of
-  ## ARM's "big.LITTLE" design concept that splits a CPU into power-efficient
-  ## and high-performance cores, the idea being that an OS will schedule threads
-  ## to the appropriate core types based on what the user currently is doing.
-  ##
-  ## Whilst big.LITTLE nominally guaranteed that both types of cores in a system
-  ## support the same instructions, newer takes on the big.LITTLE concept, for
-  ## example, Samsung's Exynos 9600 and Intel's Alder Lake, do not follow this
-  ## principle.
-  ##
-  ## If no relevant OS-level countermeasures exist, then user applications run
-  ## the risk of crashing via illegal opcode exceptions; in the best case
-  ## applications are forcibly repinned onto "known-good" cores that can
-  ## execute the instructions in question, leading to potential performance
-  ## issues as multiple threads of the same application compete with each other.
-  cached features.allIt(it == 0 or it.popcount == cpuCount)
-
 proc isHypervisorPresent*() :bool {.inline.} =
   ## **(x86 Only)**
   ##
@@ -209,13 +172,6 @@ proc hasSimultaneousMultithreading*() :bool {.inline.} =
   ## Reports `true` if the hardware is utilizing simultaneous multithreading
   ## (branded as *"hyperthreads"* on Intel processors).
   cached (features[Hyperthreading] or not features[NoSMT]).testBit(0)
-
-template onThread(feature :X86Feature; affinity :int) :bool =
-  when compileOption("rangeChecks"):
-    if affinity notin 0 ..< cpuCount:
-      raise IndexError.newException(
-        "Requested affinity greater than number of logical processors")
-  features[feature].testBit(affinity)
 
 proc hasIntelVTX*(affinity = currentAffinity()) :bool {.inline.} =
   ## **(x86 Only)**
